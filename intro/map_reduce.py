ea84fa705bf377b9681f1180812e49d9e465d50e
Patryk Koniarczyk <patrykmrtv@gmail.com>
2022-07-11 15:29:28 +0200

    test

---
+++ intro/map_reduce.py
@@ -0,0 +1,12 @@
+from mrjob.job import MRJob
+
+
+class MRWordCount(MRJob):
+
+    def mapper(self, _, line):
+        yield ['chars', len(line)]
+
+
+if __name__ == '__main__':
+
+    MRWordCount.run()
