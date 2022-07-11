fe56493f2d30062a17fc4e399446e14586e49b64
Patryk Koniarczyk <patrykmrtv@gmail.com>
2022-07-11 20:09:58 +0200

    illiada - most common word

--- Spam_Collection/spam_job.py
+++ Spam_Collection/spam_job.py
@@ -1,13 +1,24 @@
 from mrjob.job import MRJob
+from mrjob.step import MRStep
 
 
 class MRSimpleJob(MRJob):
 
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper,
+                   reducer=self.reducer)
+        ]
+
     def mapper(self, _, value):
         yield 'line', 1
+        yield 'words', len(value.split())
+        yield 'chars', len(value)
 
     def reducer(self, key, values):
         yield key, sum(values)
+
+
 if __name__ == '__main__':
 
     MRSimpleJob.run()
